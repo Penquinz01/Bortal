298b210690200e54108e19dbd0a21f8813d0dcc9
Janbaas Jamal K K <41532382+Penquinz01@users.noreply.github.com>
2025-01-25 12:13:04 +0530

    changes

--- Assets/Scripting/Player/PlayerShooting.cs
+++ Assets/Scripting/Player/PlayerShooting.cs
@@ -14,8 +14,8 @@ public class PlayerShooting : MonoBehaviour
     //currentBullet Code
     // 0 -> normal
     //1 ->impact
-    //2 -? grivity
-    // 3->magnetic
+    //2 -> grivity
+    //3->magnetic
     [SerializeField] private int totaltypeofBullet = 2;
     private float bulletTime = 0;
     private void Start()
